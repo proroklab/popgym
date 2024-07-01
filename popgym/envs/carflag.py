9a71557ec1ed36cfd1ca94f579d3b849472272ba
Ashok Arora <arora8715@gmail.com>
2024-07-01 11:12:13 +0530

    Adds easy, medium and hard levels
    The easy level has the range [-1, 1].
    The medium level has the range [-3, 3].
    The hard level has the range [-5, 5].

--- popgym/envs/carflag.py
+++ popgym/envs/carflag.py
@@ -6,6 +6,8 @@ its velocity at each timestep, and the goal flag's location when it reaches
 the oracle flag. The agent's actions alter its velocity: it can accelerate left, 
 perform a no-op (maintain current velocity), or accelerate right."""
 
+from typing import Any, Dict, Optional, Tuple
+
 import gymnasium as gym
 import numpy as np
 
@@ -13,12 +15,12 @@ from popgym.core.env import POPGymEnv
 
 
 class CarFlag(POPGymEnv):
-    """Car Flag tasks a car with driving across a 1D line to the correct flag. 
+    """Car Flag tasks a car with driving across a 1D line to the correct flag.
 
-    The car must first drive to the oracle flag and then to the correct endpoint. 
-    The agent's observation is a vector of three floats: its position on the line, 
-    its velocity at each timestep, and the goal flag's location when it reaches 
-    the oracle flag. The agent's actions alter its velocity: it can accelerate left, 
+    The car must first drive to the oracle flag and then to the correct endpoint.
+    The agent's observation is a vector of three floats: its position on the line,
+    its velocity at each timestep, and the goal flag's location when it reaches
+    the oracle flag. The agent's actions alter its velocity: it can accelerate left,
     perform a no-op (maintain current velocity), or accelerate right.
 
     Args:
@@ -27,8 +29,21 @@ class CarFlag(POPGymEnv):
     Returns:
         A gym environment
     """
-    def __init__(self, discrete: bool):
-        self.max_position = 1.1
+
+    def __init__(self, discrete=True, difficulty="easy"):
+        assert difficulty in ["easy", "medium", "hard"]
+        if difficulty == "easy":
+            self.heaven_position = 1.0
+            self.hell_position = -1.0
+        elif difficulty == "medium":
+            self.heaven_position = 3.0
+            self.hell_position = -3.0
+        elif difficulty == "hard":
+            self.heaven_position = 5.0
+            self.hell_position = -5.0
+        else:
+            raise NotImplementedError(f"Invalid difficulty {difficulty}")
+        self.max_position = self.heaven_position + 0.1
         self.min_position = -self.max_position
         self.max_speed = 0.07
 
@@ -124,7 +139,13 @@ class CarFlag(POPGymEnv):
 
         return self.state, env_reward, done, {"is_success": env_reward > 0}
 
-    def reset(self):
+    def reset(
+        self,
+        *,
+        seed: Optional[int] = None,
+        options: Optional[dict] = None,
+    ) -> Tuple[gym.core.ObsType, Dict[str, Any]]:
+        super().reset(seed=seed)
         # Randomize the heaven/hell location
         if self.np_random.integers(low=0, high=2, size=1) == 0:
             self.heaven_position = 1.0
@@ -134,11 +155,50 @@ class CarFlag(POPGymEnv):
         self.hell_position = -self.heaven_position
 
         self.state = np.array([self.np_random.uniform(low=-0.2, high=0.2), 0, 0.0])
-        return np.array(self.state)
+        return np.array(self.state), {}
 
     def get_state(self):
         # Return the position of the car, oracle, and goal
-        return self.state, self.oracle_position, self.heaven_position, self.hell_position
+        return (
+            self.state,
+            self.oracle_position,
+            self.heaven_position,
+            self.hell_position,
+        )
 
     def render(self):
         return None
+
+
+if __name__ == "__main__":
+    e = CarFlag()
+    obs = e.reset()
+    e.render()
+    while not done:
+        action = np.array(input("Enter action: ")).astype(np.int8)
+        obs, reward, done, info = e.step(action)
+        print(f"reward = {reward}")
+
+
+class CarFlagEasy(CarFlag):
+    """Car Flag tasks a car with driving across a 1D line to the correct flag.
+    The easy level has the range [-1, 1]."""
+
+    def __init__(self):
+        super().__init__("easy")
+
+
+class CarFlagMedium(CarFlag):
+    """Car Flag tasks a car with driving across a 1D line to the correct flag.
+    The medium level has the range [-3, 3]."""
+
+    def __init__(self):
+        super().__init__("medium")
+
+
+class CarFlagHard(CarFlag):
+    """Car Flag tasks a car with driving across a 1D line to the correct flag.
+    The hard level has the range [-5, 5]."""
+
+    def __init__(self):
+        super().__init__("hard")
