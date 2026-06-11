Fix count recall off-by-one action space

popgym/envs/count_recall.py
@@ -72,7 +72,7 @@ class CountRecall(POPGymEnv):
         )
         self.last_obs = np.array([0, 0])
 
-        self.action_space = gym.spaces.Discrete(self.max_card_count)
+        self.action_space = gym.spaces.Discrete(self.max_card_count + 1)
         self.max_episode_length = self.value_deck.num_cards - 1
         self.error_clamp = error_clamp
         self.reward_scale = 1 / self.max_episode_length

tests/test_count_recall.py
@@ -23,8 +23,9 @@ class TestCountRecall(AbstractTest.POPGymTest):
             obs, rew, terminated, truncated, info = self.env.step(action)
             d, q = obs
             counts[d] += 1
-            action = np.array([counts[q]])
+            action = np.array(counts[q])
             self.assertEqual(rew, 1 / (self.env.value_deck.num_cards - 1))
+            self.assertTrue(self.env.action_space.contains(action))
             reward += rew
             t += 1
 